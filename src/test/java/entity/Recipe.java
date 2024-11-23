2b7064cdd9af745e2617ed905e8e636341c866d6
norlov <norlov>
2024-11-23 15:50:39 +0300

    first test

--- src/test/java/entity/Recipe.java
+++ src/test/java/entity/Recipe.java
@@ -1,4 +1,25 @@
 package entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.Locale;
+
+@Data
 public class Recipe {
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("description")
+    private String description;
+    @JsonProperty("category")
+    private String category;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonProperty("date")
+    private LocalDateTime date;
+    @JsonProperty("ingredients")
+    private String[] ingredients = new String[]{};
+    @JsonProperty("directions")
+    private String[] directions = new String[]{};
 }
--- src/test/java/positive/RecipeControllerPositiveIntegrationTest.java
+++ src/test/java/positive/RecipeControllerPositiveIntegrationTest.java
@@ -1,4 +1,34 @@
 package positive;
 
+import entity.Recipe;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class RecipeControllerPositiveIntegrationTest {
+
+    private static final String URL = "http://localhost:8080";
+
+    @Test
+    public void getRecipeById() {
+        Recipe recipe = given()
+                .auth()
+                .basic("coookkkk@somewhere.com","$recipe_in_binary")
+                .when()
+                .contentType(ContentType.JSON)
+                .get(URL + "/api/recipe/1")
+                .then()
+                .extract()
+                .body()
+                .as(Recipe.class);
+
+        assertEquals("Fresh Mint Tea",recipe.getName());
+        assertEquals("beverage", recipe.getCategory());
+        assertEquals(LocalDateTime.parse("2024-04-30T22:34:58"), recipe.getDate());
+    }
+
 }
